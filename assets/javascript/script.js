8b0df68afe8893e0ba3c5f3e049279845141949c
M Ryan <mxryan@live.com>
2018-09-22 02:15:05 -0400

    basic logic finished

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,7 +5,7 @@ var searchBar = document.querySelector("#search-bar");
 var subBtn = document.querySelector("#submit-btn");
 var gifDisplay = document.querySelector("#gif-area");
 
-function grabGIFs(searchTerm){
+function grabGIFs(searchTerm) {
   var baseUrl = "http://api.giphy.com/v1/gifs/search?q=";
   var apiKey = "&api_key=VxlTnIrmRXycSQQNUh8bYJ7vrKAxBDSB";
   var query = baseUrl + searchTerm + apiKey + "&limit=9";
@@ -13,12 +13,12 @@ function grabGIFs(searchTerm){
   req.onload = () => {
     var data = JSON.parse(req.responseText);
     console.log(data.data);
-    
+
     for (var i = 0; i < data.data.length; i++) {
       var x = data.data[i].images
-      addGif(x.original_still.url, x.original.url);
+      addGif(x.original_still.url, x.original.url, i);
     }
-    
+
   };
   req.open("GET", query, true);
   req.send();
@@ -28,21 +28,33 @@ grabGIFs("turtle");
 function addBtn(searchTerm) {
   var btn = document.createElement("button");
   btn.value = searchTerm;
+  console.log(searchTerm);
   btn.textContent = searchTerm;
   btn.classList.add("search-button");
-  btn.addEventListener("click", ()=>{
-    grabGIFs(this.value);
+  btn.addEventListener("click", (e) => {
+    console.log(e.target.value);
+    grabGIFs(e.target.value);
   });
   btnDisplay.appendChild(btn);
 }
 
-function addGif(stillURL, gifURL) {
+function addGIFS(gifArr) {
+
+}
+
+function addGif(stillURL, gifURL, i) {
+  //break this into separate 'remove' fn?
+  if (i === 0) {
+    while (gifDisplay.firstChild) {
+      gifDisplay.removeChild(gifDisplay.firstChild);
+    }
+  }
   var img = document.createElement("img");
   img.src = stillURL;
   img.dataset.stillURL = stillURL;
   img.dataset.gifURL = gifURL;
   img.classList.add("gif");
-  
+
   img.addEventListener("click", swap);
   var gifBox = document.createElement("div");
   gifBox.classList.add("gif-container");
@@ -53,15 +65,15 @@ function addGif(stillURL, gifURL) {
 
 function swap() {
   //swaps between still and gif
-  console.log("swap called");
+
   var d = this.dataset;
   this.src == d.stillURL ? this.src = d.gifURL : this.src = d.stillURL;
-  
-  
-  
+
+
+
 }
 
-subBtn.addEventListener("click", ()=> {
+subBtn.addEventListener("click", () => {
   if (searchBar.value) {
     addBtn(searchBar.value);
   }
